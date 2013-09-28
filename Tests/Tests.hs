07bb3c5fec20577b6c94a91a3c62a725a3becb27
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2013-09-28 14:14:17 +0100

    Add final consistency test case from RFC6962

--- Tests/Tests.hs
+++ Tests/Tests.hs
@@ -11,7 +11,8 @@ main = defaultMain tests
 
 tests = [ testGroup "Consistency proof examples in RFC6962" consistencyProofsInRfc6962]
 
-consistencyProofsInRfc6962 = [testCase "6 7" testFoo]
-
-testFoo = proof 6 7 @?= [6,7,2]
-testBar = proof 3 7 @?= [3,4,5]
+consistencyProofsInRfc6962 = [
+                               testCase "6 7" $ proof 6 7 @?= [6,7,2]
+                             , testCase "4 7" $ proof 4 7 @?= [3]
+                             , testCase "3 7" $ proof 3 7 @?= [10,11,4,3]
+                             ]
