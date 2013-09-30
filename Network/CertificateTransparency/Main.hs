066f3efa765da2f96b88d1877e80cec84f81ea8c
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2013-09-30 16:47:01 +0100

    Vaguely more understandable code?

--- Network/CertificateTransparency/Main.hs
+++ Network/CertificateTransparency/Main.hs
@@ -8,6 +8,7 @@ import Control.Monad
 import Control.Monad.Loops (whileM_)
 import Control.Monad.Maybe
 import Control.Monad.Trans
+import Data.Ord
 import Data.IORef
 import Network.CertificateTransparency.LogServerApi
 import Network.CertificateTransparency.Types
@@ -15,7 +16,7 @@ import Network.CertificateTransparency.Verification
 import System.Log.Handler.Syslog
 import System.Log.Logger
 
-old = SignedTreeHead
+knownGoodSth = SignedTreeHead
     { treeSize = 1979426
     , timestamp = 1368891548960
     , rootHash = B64.decodeLenient "8UkrV2kjoLcZ5fP0xxVtpsSsWAnvcV8aPv39vh96J2o="
@@ -24,46 +25,46 @@ old = SignedTreeHead
 
 main = do
     setupLogging
-    ref <- newIORef $ Just (old, Just True)
 
-    whileM_ (notFoundBadSth ref) $ do
-        sth <- readIORef ref
-        case sth of
-            Just (sth', _) -> do
-                next <- oneIteration sth'
-                debugM "main" $ "Iteration complete: " ++ show next
-                writeIORef ref next
+    sthRef <- newIORef (knownGoodSth, Just True)
+    whileM_ (notFoundBadSth sthRef) $ do
+        sth <- readIORef sthRef
+        sthToStore <- updateAndCheck sth
+        writeIORef sthRef sthToStore
+
         threadDelay (2*60*1000*1000) -- every 2 minutes
 
-    badSth <- readIORef ref
+    badSth <- readIORef sthRef
     errorM "main" $ "The following STH failed its consistency check: " ++ show badSth
 
-setupLogging :: IO ()
-setupLogging = do
-    removeAllHandlers
-    s <- openlog "ct-consistency-checker" [PID] DAEMON DEBUG
-    updateGlobalLogger rootLoggerName (addHandler s)
-    updateGlobalLogger rootLoggerName (setLevel DEBUG)
-    infoM "main" "Logger started."
-
-notFoundBadSth :: IORef (Maybe (SignedTreeHead, Maybe Bool)) -> IO Bool
-notFoundBadSth ref = do
-    sth <- readIORef ref
-    return $ shouldContinue sth
-
-shouldContinue :: Maybe (SignedTreeHead, Maybe Bool) -> Bool
-shouldContinue (Just (sth, Just True))  = True
-shouldContinue (Just (sth, Just False)) = False
-shouldContinue (Just (sth, Nothing))    = True
-shouldContinue Nothing                = True
-
-
-
-oneIteration :: SignedTreeHead -> IO (Maybe (SignedTreeHead, Maybe Bool))
-oneIteration old = runMaybeT $ MaybeT getSth >>= lift . (checkConsistency old)
     where
-        checkConsistency :: SignedTreeHead -> SignedTreeHead -> IO (SignedTreeHead, Maybe Bool)
-        checkConsistency old new = do
-            consProof <- getSthConsistency old new
-            return (new, checkConsistencyProof old new <$> consProof)
+        setupLogging :: IO ()
+        setupLogging = do
+            removeAllHandlers
+            s <- openlog "ct-consistency-checker" [PID] DAEMON DEBUG
+            updateGlobalLogger rootLoggerName (addHandler s)
+            updateGlobalLogger rootLoggerName (setLevel DEBUG)
+            infoM "main" "Logger started."
 
+        notFoundBadSth :: IORef (SignedTreeHead, Maybe Bool) -> IO Bool
+        notFoundBadSth ref = do
+            sth <- readIORef ref
+            return $ shouldContinue sth
+                where
+                    shouldContinue :: (SignedTreeHead, Maybe Bool) -> Bool
+                    shouldContinue (sth, Just b)  = b
+                    shouldContinue (sth, Nothing) = True
+
+        updateAndCheck :: (SignedTreeHead, Maybe Bool) -> IO (SignedTreeHead, Maybe Bool)
+        updateAndCheck (prevSth, prevB) = do
+            nextSth' <- getSth
+            case nextSth' of
+                Just nextSth -> if (comparing treeSize prevSth nextSth == LT)
+                                then do
+                                    consProof <- getSthConsistency prevSth nextSth
+                                    let r = (nextSth,
+                                             checkConsistencyProof prevSth nextSth <$> consProof)
+                                    debugM "main" $ "Iteration: " ++ show r
+                                    return r
+                                else return (prevSth, Nothing)
+                Nothing      -> return (prevSth, prevB)
