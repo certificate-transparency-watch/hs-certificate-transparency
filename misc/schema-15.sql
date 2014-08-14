8fe620deb3042d1d5980333fb6166a52fedf4bfc
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2014-08-14 17:31:12 +0000

    Add alpha.ctlogs.org
    https://github.com/certificate-transparency-watch/hs-certificate-transparency/issues/25

---
+++ misc/schema-15.sql
@@ -0,0 +1,2 @@
+INSERT INTO log_server (id, prefix, name) VALUES (3, 'alpha.ctlogs.org', 'alpha.ctlogs.org');
+INSERT INTO log_entry (log_server_id, idx, domain, cert_md5, log_entry_type) VALUES (3, -1, 'UNKNOWN', '\x', 0);
