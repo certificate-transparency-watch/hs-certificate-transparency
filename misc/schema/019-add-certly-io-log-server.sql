cc06882b3273d6fb38f79f66585da0e0b056868a
Tom Fitzhenry <git@tom-fitzhenry.me.uk>
2014-12-24 04:11:32 +0000

    Add certly.io

---
+++ misc/schema/019-add-certly-io-log-server.sql
@@ -0,0 +1,3 @@
+INSERT INTO log_server (id, prefix, name) VALUES (6, 'log.certly.io', 'log.certly.io');
+
+INSERT INTO log_entry (log_server_id, idx, domain, cert_md5, log_entry_type) VALUES (6, -1, 'UNKNOWN', '\x', 0);
