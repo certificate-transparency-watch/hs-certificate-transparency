a516e9a9fc5732631bb569fd17e919cbb42ef67e
Tom Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-24 20:38:59 +0100

    Add Symantec and Venafi log servers
    Closes
    https://github.com/certificate-transparency-watch/hs-certificate-transparency/issues/33

---
+++ misc/schema/027-add-symantec-log-server.sql
@@ -0,0 +1,2 @@
+INSERT INTO log_server (id, prefix, name) VALUES (7, 'ct.ws.symantec.com', 'ct.ws.symantec.com');
+INSERT INTO log_entry (log_server_id, idx, domain, cert_md5, log_entry_type) VALUES (7, -1, 'UNKNOWN', '\x', 0);
---
+++ misc/schema/028-add-venafi-log-server.sql
@@ -0,0 +1,2 @@
+INSERT INTO log_server (id, prefix, name) VALUES (8, 'ctlog.api.venafi.com', 'ctlog.api.venafi.com');
+INSERT INTO log_entry (log_server_id, idx, domain, cert_md5, log_entry_type) VALUES (8, -1, 'UNKNOWN', '\x', 0);
