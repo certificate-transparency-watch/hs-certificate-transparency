0ba4c24ec26f66975d41c75ebc649aafc09b4c8d
Thomas Fitzhenry <tom@tom-fitzhenry.me.uk>
2015-09-24 13:58:59 +0100

    Refactor: Remove unneccesary code from HTTP code

--- Network/CertificateTransparency/LogServerApi.hs
+++ Network/CertificateTransparency/LogServerApi.hs
@@ -6,52 +6,27 @@ module Network.CertificateTransparency.LogServerApi
     ) where
 
 import Data.Aeson
-import qualified Data.ByteString.Lazy.Char8 as BSLC
 import Network.HTTP.Conduit
 import Network.HTTP.Types()
 import Network.HTTP.Types.Header()
-import System.Log.Logger (debugM)
 
 import Network.CertificateTransparency.Parser()
 import Network.CertificateTransparency.Types
 
 getSth :: LogServer -> IO (Maybe SignedTreeHead)
 getSth logServer = do
-    initReq <- parseUrl $ "https://" ++ logServerPrefix logServer ++ "/ct/v1/get-sth"
-
-    let req' = initReq { secure = True
-                       , method = "GET"
-                       }
-    res <- withManager $ httpLbs req'
-
-    let rawNewSth =  responseBody res
-
-    debugM "get-sth" $ BSLC.unpack rawNewSth
-
-    return $ decode rawNewSth
-
+    req <- parseUrl $ "https://" ++ logServerPrefix logServer ++ "/ct/v1/get-sth"
+    res <- withManager $ httpLbs req
+    return . decode . responseBody $ res
 
 getSthConsistency :: LogServer -> SignedTreeHead -> SignedTreeHead -> IO (Maybe ConsistencyProof)
 getSthConsistency logServer h1 h2 = do
-    let url = "https://" ++ logServerPrefix logServer ++ "/ct/v1/get-sth-consistency?first=" ++ show (treeSize h1) ++ "&second=" ++ show (treeSize h2)
-    initReq <- parseUrl url
-
-    res <- withManager $ httpLbs initReq
-
-    let r = responseBody res
-
-    debugM url $ BSLC.unpack r
-
-    return $ decode r
+    req <- parseUrl $ "https://" ++ logServerPrefix logServer ++ "/ct/v1/get-sth-consistency?first=" ++ show (treeSize h1) ++ "&second=" ++ show (treeSize h2)
+    res <- withManager $ httpLbs req
+    return . decode . responseBody $ res
 
 getEntries :: LogServer -> (Int, Int) -> IO (Maybe [LogEntry])
 getEntries logServer (start, end) = do
-    let url = "https://" ++ logServerPrefix logServer ++ "/ct/v1/get-entries?start=" ++ show start ++ "&end=" ++ show end
-
-    initReq <- parseUrl url
-
-    res <- withManager $ httpLbs initReq
-
-    let r = responseBody res
-
-    return $ logEntriesEntries <$> decode r
+    req <- parseUrl $ "https://" ++ logServerPrefix logServer ++ "/ct/v1/get-entries?start=" ++ show start ++ "&end=" ++ show end
+    res <- withManager $ httpLbs req
+    return $ logEntriesEntries <$> (decode . responseBody $ res)
